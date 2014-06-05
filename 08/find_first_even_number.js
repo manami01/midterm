b4934fa7b319b6b656df3847543f92cdca09aa53
manami01 <s13518mt@sfc.keio.ac.jp>
2014-06-05 11:50:21 +0900

    問８都中まで

--- 08/find_first_even_number.js
+++ 08/find_first_even_number.js
@@ -1,5 +1,7 @@
-var findFirstEvenNumber = function(list){ //配列って、書かなくても言い。
-	return list % 2 === 0
+var findFirstEvenNumber = function(n){ //配列って、書かなくても言い。
+if(n.length == 0){
+	return null;}
 };
 
-//なんなんだー！笑
+//a[0]だったら一番始め０番目が呼び出される
+//while文、繰り返し
